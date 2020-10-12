f3549888b641cae18dd58519da133032e3ba7c75
Alexander Perez <alexander.perez@patelhs.org>
2020-10-12 12:16:42 +0000

    Beginning questions.

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    //Beginning of Survey//
+    System.out.println("Hello, ");
+    System.out.println("Welcome to the MiniSurvey website!");
+
+
+
+
+ 
+ 
+ 
   }
 }
