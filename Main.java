ae9d957f97e8f4ad96124d3510b5e2fc4285280c
Alexander Perez <alexander.perez@patelhs.org>
2020-10-12 12:37:17 +0000

    The first question on Survey.

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     //Beginning of Survey//
-    System.out.println("Hello, ");
-    System.out.println("Welcome to the MiniSurvey website!");
+      System.out.println("Hello, ");
+      System.out.println("Welcome to the MiniSurvey website!");
+
+    //Scanner for main questions//
+      Scanner scan = new Scanner(System.in);
+
+        System.out.println("What's your FULL name?"); //First Question//
+        String name = scan.next();
+          
 
 
 
 
- 
- 
- 
+  
   }
 }
