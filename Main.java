47456a8cb9dece49c7e9671b7cda1b540b957f8a
Alexander Perez <alexander.perez@patelhs.org>
2020-10-13 17:55:21 +0000

    Third question, Scanner, loop, if/else.

--- Main.java
+++ Main.java
@@ -10,15 +10,30 @@ class Main {
 
         System.out.println("What's your FULL name?"); //First Question//
         String name = scan.next();
+        name += scan.nextLine();//Use code if more than one word//
 
         System.out.println("What is your age? Respond with number! "); //Second Question//
-        scan.nextInt();
-        String age = "18";
-        if(age == "18"){
+        int age = scan.nextInt();
+        if(age >= 18){ //If,Else Statement #1//
           System.out.println("You are not a minor!");
         }else{
           System.out.println("You are a minor!");
 
         }
+
+      Scanner number = new Scanner(System.in);
+
+        System.out.println("Which animal is better cats or dogs?");
+        String animal = scan.next();
+
+        if(animal == "dogs"){
+          for(int i = 0; i<3; i++){
+            System.out.println("You like dogs!");
+          }
+          }else{
+          for(int i = 0; i<3; i++){
+            System.out.println("You like cats!");
+          } 
+      }
+    }
   }
-}
